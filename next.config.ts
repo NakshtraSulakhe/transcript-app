750853bbecd0f6e6e97fde14b0d4e4a95ba62fd4
TusharPhalak44 <tushar@tarajglobal.com>
2026-09-24 17:58:34 +0530

    24-0926old

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "172.16.16.170",
+    "172.16.16.170:3000",
+    "localhost",
+    "localhost:3000",
+    "127.0.0.1",
+    "127.0.0.1:3000",
+  ],
 };
 
 export default nextConfig;
